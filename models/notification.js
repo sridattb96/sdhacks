35eca86975b0174b37323e2de5274b87be2435f8
Chang Liu <shampliu@gmail.com>
2015-10-04 04:43:00 -0700

    notifications dummy data

--- models/notification.js
+++ models/notification.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema; 
 
 var NotificationSchema = new Schema({
-	
+	message: String,
+	time: Date
+
 });
 
 var Notification = mongoose.model('Notification', NotificationSchema);
